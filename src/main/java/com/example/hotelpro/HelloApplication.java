09112633dcf87e6cf1857daac256cbad9bebcc54
minhAptechGitHub <minh.dd.2417@aptechlearning.edu.vn>
2025-02-20 22:46:29 +0700

    ve dashboard
    co mot vai phan do to khong quen dung git nen bi sua them

--- src/main/java/com/example/hotelpro/HelloApplication.java
+++ src/main/java/com/example/hotelpro/HelloApplication.java
@@ -1,19 +1,34 @@
 package com.example.hotelpro;
 
+import connect.Connect;
+import connect.Migration;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
+        Connection connection = Connect.connection();
+        if (connection == null) {
+            System.out.println("Không tìm thấy cơ sở dữ liệu!");
+        } else {
+            if (!Connect.checkTable("Customer")) {
+                Migration.migrate();
+            } else {
+                System.out.println("Cơ sở dữ liệu đã tồn tại!");
+            }
+        }
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/hotelpro/login/login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        scene.getStylesheets().add(getClass().getResource("/css/main.css").toExternalForm());
         stage.show();
     }
 
