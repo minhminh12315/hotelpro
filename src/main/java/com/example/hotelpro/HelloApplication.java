a4f7e8fb3e20a1943c50a6dd8d5bc1665b57ac5e
Hannh-ff <haiem4259@gmail.com>
2025-03-04 10:19:26 +0700

    chuyển trang

--- src/main/java/com/example/hotelpro/HelloApplication.java
+++ src/main/java/com/example/hotelpro/HelloApplication.java
@@ -2,10 +2,12 @@ package com.example.hotelpro;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
--- src/main/java/com/example/hotelpro/HelloController.java
+++ src/main/java/com/example/hotelpro/HelloController.java
@@ -3,71 +3,93 @@ package com.example.hotelpro;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HelloController {
 
     @FXML
-    private CheckBox chkFood;
+    private CheckBox chkFood, chkDrink, chkSnack, chkRepair, chkBed, chkRoomDecor;
     @FXML
-    private CheckBox chkDrink;
+    private CheckBox chkRoomSauna, chkSpa, chkAirport, chkCheckinout, chkKaraoke;
     @FXML
-    private CheckBox chkSnack;
-    @FXML
-    private CheckBox chkRepair;
-    @FXML
-    private CheckBox chkBed;
-    @FXML
-    private CheckBox chkRoomDecor;
-    @FXML
-    private CheckBox chkRoomSauna;
-    @FXML
-    private CheckBox chkSpa;
-    @FXML
-    private CheckBox chkAirport;
-    @FXML
-    private CheckBox chkCheckinout;
-    @FXML
-    private CheckBox chkKaraoke;
-    @FXML
-    private CheckBox chkFishing;
-    @FXML
-    private CheckBox chkBoating;
-    @FXML
-    private CheckBox chkBiking;
+    private CheckBox chkFishing, chkBoating, chkBiking;
     @FXML
     private ListView<String> cartlistView;
-
     private final ObservableList<String> selectedServices = FXCollections.observableArrayList();
+    private final Map<String, Stage> openStages = new HashMap<>();
 
     @FXML
     public void initialize() {
         cartlistView.setItems(selectedServices);
+        setupMultiInstanceCheckbox(chkFood, "Food");
+        setupMultiInstanceCheckbox(chkDrink, "Drink");
+        setupMultiInstanceCheckbox(chkSnack, "Snack");
 
-        setupCheckboxListener(chkFood, "Food");
-        setupCheckboxListener(chkDrink, "Drink");
-        setupCheckboxListener(chkSnack, "Snack");
-        setupCheckboxListener(chkRepair, "Furniture Repair");
-        setupCheckboxListener(chkBed, "Bed Sheets");
-        setupCheckboxListener(chkRoomDecor, "Room Decoration");
-        setupCheckboxListener(chkRoomSauna, "Sauna");
-        setupCheckboxListener(chkSpa, "Spa");
-        setupCheckboxListener(chkAirport, "Airport Pickup");
-        setupCheckboxListener(chkCheckinout, "Late Check-in");
-        setupCheckboxListener(chkKaraoke, "Karaoke");
-        setupCheckboxListener(chkFishing, "Fishing");
-        setupCheckboxListener(chkBoating, "Boating");
-        setupCheckboxListener(chkBiking, "Biking");
+        setupToggleCheckbox(chkSpa, "Spa");
+        setupToggleCheckbox(chkRepair, "Furniture Repair");
+        setupToggleCheckbox(chkBed, "Bed Sheets");
+        setupToggleCheckbox(chkRoomDecor, "Room Decoration");
+        setupToggleCheckbox(chkRoomSauna, "Sauna");
+        setupToggleCheckbox(chkAirport, "Airport Pickup");
+        setupToggleCheckbox(chkCheckinout, "Late Check-in");
+        setupToggleCheckbox(chkKaraoke, "Karaoke");
+        setupToggleCheckbox(chkFishing, "Fishing");
+        setupToggleCheckbox(chkBoating, "Boating");
+        setupToggleCheckbox(chkBiking, "Biking");
     }
 
-    private void setupCheckboxListener(CheckBox checkBox, String serviceName) {
-        checkBox.selectedProperty().addListener((obs, wasSelected, isNowSelected) -> {
-            if (isNowSelected) {
+    private void setupMultiInstanceCheckbox(CheckBox checkBox, String serviceName) {
+        checkBox.setOnMouseClicked(event -> {
+            selectedServices.add(serviceName);
+            openPageMulti(serviceName);
+            checkBox.setSelected(false);
+        });
+    }
+
+    private void setupToggleCheckbox(CheckBox checkBox, String serviceName) {
+        checkBox.selectedProperty().addListener((obs, oldVal, newVal) -> {
+            if (newVal) {
                 selectedServices.add(serviceName);
             } else {
                 selectedServices.remove(serviceName);
             }
         });
+    }
+
+    private void openPageMulti(String serviceName) {
+        String fxmlFile = getFXMLForService(serviceName);
+        if (fxmlFile == null) return;
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(fxmlFile));
+            Scene scene = new Scene(root, 850, 600);
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle(serviceName + " Services");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getFXMLForService(String serviceName) {
+        switch (serviceName) {
+            case "Food":
+                return "/com/example/hotelpro/food-selection.fxml";
+            case "Drink":
+                return "/com/example/hotelpro/water-selection.fxml";
+            case "Snack":
+                return "/com/example/hotelpro/snack.fxml";
+            default:
+                return null;
+        }
     }
 }
